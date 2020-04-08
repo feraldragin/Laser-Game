985f9bb3bece69f8769157a9e41751beeebcbff2
iep1901 <iep1901@rit.edu>
2020-04-07 20:27:42 -0700

    Created methods for Safe.java. Implemented safeDisplay() method as well as created a constructor for Safe.java.

--- src/lasers/model/Safe.java
+++ src/lasers/model/Safe.java
@@ -1,4 +1,49 @@
 package lasers.model;
 
 public class Safe {
+
+    private String[][] safeArray;
+
+    public Safe(String[][] safeArray){
+        this.safeArray = safeArray;
+    }
+
+    public String safeAdd(){
+        //adds if laserCheck() is true and sends error if false
+        return "add";
+    }
+
+    public String safeRemove(){
+        return "remove";
+    }
+
+    public void safeDisplay(){
+        System.out.print("  ");
+        for (int column = 0; column < safeArray[0].length; column++){
+            System.out.print(column + " ");
+        }
+        System.out.println();
+        System.out.print("  ");
+        for (int i = 0; i < (safeArray[0].length*2)-1; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+        for (int row = 0; row < safeArray.length; row++){
+            System.out.print((row) + "|");
+            for (int column = 0; column < safeArray[row].length; column++){
+                System.out.print(safeArray[row][column] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public boolean safeVerify(){
+        return false;
+    }
+
+    public boolean laserCheck(){
+        //if pillars are good and laser, number, letter, and/or * not in place
+        return false;
+    }
 }
--- src/lasers/ptui/LasersPTUI.java
+++ src/lasers/ptui/LasersPTUI.java
@@ -2,17 +2,45 @@ package lasers.ptui;
 
 import lasers.model.Safe;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LasersPTUI {
     /**
      * The main method
      * @param args command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // check sanity of input
         if (args.length < 1 || args.length > 2) {
             System.out.println("Usage: java LasersPTUI safe-file [input]");
         } else {
             // TODO
+            if (args.length == 1){
+                int lines = 0;
+                File file = new File(args[0]);
+                Scanner in = new Scanner(file);
+                String first = in.nextLine();
+                String[] dim = first.split(" ");
+                int rows = Integer.parseInt(dim[0]);
+                int columns = Integer.parseInt(dim[1]);
+                String[][] safeArray = new String[rows][columns];
+                while (in.hasNextLine()){
+                    String line = in.nextLine();
+                    lines++;
+                    String[] parts = line.split(" ");
+                    if(parts[0].isEmpty()){
+                        break;
+                    }
+                    for(int i = 0; i < columns;i++){
+                        safeArray[lines-1][i] = parts[i];
+                    }
+                }
+                Safe safe = new Safe(safeArray);
+                safe.safeDisplay();
+
+            }
         }
     }
 }
